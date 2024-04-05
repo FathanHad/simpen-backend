9f63a2b25c4a029b921e163371327545b108ab75
rafighalibin <rafighalibin@ui.ac.id>
2024-04-05 14:33:56 +0700

    implement ganti pengajar to get all permintaan perubahan

--- src/main/java/com/nakahama/simpenbackend/PerubahanKelas/controller/PermintaanPerubahanController.java
+++ src/main/java/com/nakahama/simpenbackend/PerubahanKelas/controller/PermintaanPerubahanController.java
@@ -5,9 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.nakahama.simpenbackend.PerubahanKelas.service.GantiPengajarService;
 import com.nakahama.simpenbackend.PerubahanKelas.service.RescheduleService;
 import com.nakahama.simpenbackend.util.ResponseUtil;
 import com.nakahama.simpenbackend.PerubahanKelas.dto.PermintaanPerubahanMapper;
+import com.nakahama.simpenbackend.PerubahanKelas.model.PengajarMenggantikan;
 import com.nakahama.simpenbackend.PerubahanKelas.model.Reschedule;
 
 import java.util.*;
@@ -19,11 +22,16 @@ public class PermintaanPerubahanController {
     @Autowired
     RescheduleService rescheduleService;
 
+    @Autowired
+    GantiPengajarService gantiPengajarService;
+
     @GetMapping("")
-    public ResponseEntity<Object> getAllRescheduleByKelas() {
-        // TODO: Implement ganti Pengajar
-        List<Reschedule> response = rescheduleService.getAll();
-        return ResponseUtil.okResponse(PermintaanPerubahanMapper.toListReadPermintaanPerubahan(response), "Success");
+    public ResponseEntity<Object> getAllPermintaanPerubahan() {
+        List<Reschedule> listReschedule = rescheduleService.getAll();
+        List<PengajarMenggantikan> listPengajarMenggantikan = gantiPengajarService.getAll();
+        return ResponseUtil.okResponse(
+                PermintaanPerubahanMapper.toListReadPermintaanPerubahan(listReschedule, listPengajarMenggantikan),
+                "Success");
     }
 
 }
--- src/main/java/com/nakahama/simpenbackend/PerubahanKelas/dto/PermintaanPerubahanMapper.java
+++ src/main/java/com/nakahama/simpenbackend/PerubahanKelas/dto/PermintaanPerubahanMapper.java
@@ -3,14 +3,33 @@ package com.nakahama.simpenbackend.PerubahanKelas.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.nakahama.simpenbackend.PerubahanKelas.model.PengajarMenggantikan;
 import com.nakahama.simpenbackend.PerubahanKelas.model.Reschedule;
 
 public class PermintaanPerubahanMapper {
-    public static List<ReadPermintaanPerubahan> toListReadPermintaanPerubahan(List<Reschedule> listReschedule) {
+    public static List<ReadPermintaanPerubahan> toListReadPermintaanPerubahan(List<Reschedule> listReschedule,
+            List<PengajarMenggantikan> listPengajarMenggantikan) {
         List<ReadPermintaanPerubahan> response = new ArrayList<>();
         for (Reschedule reschedule : listReschedule) {
             response.add(toReadDetailReschedule(reschedule));
         }
+
+        for (PengajarMenggantikan pengajarMenggantikan : listPengajarMenggantikan) {
+            response.add(toReadDetailGantiPengajar(pengajarMenggantikan));
+        }
+        return response;
+    }
+
+    private static ReadPermintaanPerubahan toReadDetailGantiPengajar(PengajarMenggantikan pengajarMenggantikan) {
+        ReadPermintaanPerubahan response = new ReadPermintaanPerubahan();
+        response.setPermintaanId(pengajarMenggantikan.getId());
+        response.setKelasId(pengajarMenggantikan.getSesiKelas().getKelas().getKelasId());
+        response.setNamaPengajar(pengajarMenggantikan.getSesiKelas().getPengajar().getNama());
+        response.setPengajarId(pengajarMenggantikan.getSesiKelas().getPengajar().getId());
+        response.setStatus(pengajarMenggantikan.getStatus());
+        response.setWaktuPermintaan(pengajarMenggantikan.getWaktuPermintaan());
+        response.setTipePermintaan("Ganti Pengajar");
+
         return response;
     }
 
